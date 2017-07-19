35d8465fa27230da302d6658b8676c0dd8be6d91
Daniel Clough <daniel.alan.clough@gmail.com>
2017-07-18 20:08:48 -0700

    Create sendEmail.php

---
+++ sendEmail.php
@@ -0,0 +1,17 @@
+<?php
+// If you are using Composer (recommended)
+require 'vendor/autoload.php';
+
+$from = new SendGrid\Email("Example User", "help@totalfinance.herokuapp.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "daniel@paceifictech.us");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+print_r($response->headers());
+echo $response->body();
