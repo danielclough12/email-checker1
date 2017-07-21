b7a06737afa4fed2f0e68a02998e32b3cb19372d
Daniel Clough <daniel.alan.clough@gmail.com>
2017-07-20 22:10:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ Hello world?
 <form class="register-form">
       <input id="name_set" type="text" name="userName" placeholder="Name"/>
       <input id="email_set" type="text" name="userEmail" placeholder="Email address"/>
-      <button type="button" class="w3-button w3-black" id="signin_now" ahref="sendEmail.php>register</button>
+      <button type="button" class="w3-button w3-black" id="signin_now" ahref="sendEmail.php">register</button>
 </form>
 </html>
