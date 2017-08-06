3df06bb217727ee55a11b4126a0b5580907d3706
Daniel Clough <daniel.alan.clough@gmail.com>
2017-08-06 00:04:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 require 'vendor/autoload.php';
 // If you are not using Composer
 // require("path/to/sendgrid-php/sendgrid-php.php");
-$from = new SendGrid\Email("Example User", "test@example.com");
+$from = new SendGrid\Email("Example User", "daniel@pacifictech.us");
 $subject = "Sending with SendGrid is Fun";
-$to = new SendGrid\Email("Example User", "test@example.com");
+$to = new SendGrid\Email("Example User", "daniel.alan.clough@gmail.com");
 $content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 $apiKey = getenv('SENDGRID_API_KEY');
@@ -16,4 +16,3 @@ $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 print_r($response->headers());
 echo $response->body();
-?>
