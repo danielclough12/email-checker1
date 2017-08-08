996ed75e49c8b268b3d69f81ed5bb471f74133d5
Daniel Clough <daniel.alan.clough@gmail.com>
2017-08-07 18:10:21 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
-
-if(mail('daniel.alan.clough@gmail.com', 'hello', 'hello'))
-      echo 'THIS IS CORRECT';
-else
-      echo 'This is incorrect';
-/*
-?>
-
-<?php
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
 // If you are using Composer (recommended)
-require 'vendor/autoload.php';
+//require 'vendor/autoload.php';
 $from = new SendGrid\Email("Example User", "danielclough12@email-checker1.herokuapp.com");
 $subject = "Sending with SendGrid is Fun";
 $to = new SendGrid\Email("Example User", "daniel.alan.clough@gmail.com");
@@ -19,13 +10,16 @@ $content = new SendGrid\Content("text/plain", "and easy to do anywhere, even wit
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 $apiKey = getenv('SG.FxJXt2uWTpetV4i8jwHVRw');
 $sg = new \SendGrid($apiKey);
-$response = $sg->client->mail()->send()->post($mail);
+//$response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
-print_r($response->headers());
+//print_r($response->headers());
 echo $response->body();
-$sendgrid->send($mail);
-mail($from, $subject, $to, $content);
-?>
+if($sendgrid->send($mail))
+      echo 'THIS IS CORRECT';
+else
+      echo 'This is incorrect';
+//mail($from, $subject, $to, $content);
+/*?>
 <?php
 require 'vendor/autoload.php';
 $sendgrid = new SendGrid("SG.FxJXt2uWTpetV4i8jwHVRw");
